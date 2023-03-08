8e7b0852edf6cb17ba64f97fb0fdada4c2e0b6f0
storyofus <291608987@qq.com>
2023-03-08 11:33:04 +0800

    feat: eslint和tsconfig配置调整

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,11 @@ module.exports = {
     semi: 0,
     'comma-dangle': 0,
     '@typescript-eslint/no-empty-function': 0,
+    /** 关闭方法名前必须有空格 */
+    'space-before-function-paren': 0,
+    /** 允许any */
+    '@typescript-eslint/no-explicit-any': 0,
+    /** 变量后边换行 */
+    'newline-after-var': 1,
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import monkey, { cdn } from 'vite-plugin-monkey';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     vue(),
     monkey({
